765074442b265586c3e4e94e8626e253082346d7
Bert Burgemeister <trebbu@googlemail.com>
2016-07-31 18:49:17 +0200

    Fix a few obvious bugs in keymap
    (Still untested.)

--- firmware/keymap_iso50.c
+++ firmware/keymap_iso50.c
@@ -1,42 +1,31 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* qwerty */
-    [0] = KEYMAP(
+    [0] = KEYMAP(               /* qwerty */
         ESC ,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,
         TAB ,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT,NUHS, ENT,
         LSFT,NUBS,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH,RSFT,BSPC,
-        LCTL,LGUI,LALT, FN0,                FN1, FN2, SPC, FN4,RGUI, APP,RCTL),
-    /* Num */
-    [1] = KEYMAP(
+        LCTL,LGUI,LALT, FN0, FN1,                FN2, SPC, FN3,RGUI, APP,RCTL),
+    [1] = KEYMAP(               /* Num */
         1   ,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,PPLS,
         F1  ,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, GRV,PENT,
         TRNS,  P1,  P2,  P3,  P4,  P5,  P6,  P7,  P8,  P9,  P0,PMNS,TRNS,PDOT,
-        TRNS,TRNS,TRNS,TRNS,                FN3,TRNS,PDOT,TRNS,TRNS,TRNS,TRNS),
-    /* Nav */
-    [2] = KEYMAP(
+        TRNS,TRNS,TRNS,TRNS,  NO,                 NO,NLCK,  NO,TRNS,TRNS,TRNS),
+    [2] = KEYMAP(               /* Nav */
         TRNS,  NO,  UP,  NO,  NO,  NO,  NO,PGUP,  UP,PGDN,PSCR,SLCK,PAUS,
-        NO  ,LEFT,DOWN,RGHT,  NO,  NO,HOME,LEFT,DOWN,RGHT, INS, DEL,  NO,  NO,
+        TRNS,LEFT,DOWN,RGHT,  NO,  NO,HOME,LEFT,DOWN,RGHT, INS, DEL,  NO,TRNS,
         TRNS,MUTE,VOLD,EJCT,VOLU,  NO,  NO, END,  NO,  NO,  NO,  NO,TRNS, DEL,
-        TRNS,TRNS,TRNS, FN3,               TRNS,TRNS,CAPS,TRNS,TRNS,TRNS,TRNS),
-    /* Num + Nav */
-    [3] = KEYMAP(
-        TRNS,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
-        NO  ,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,TRNS, DEL,
-        TRNS,TRNS,TRNS,TRNS,               TRNS,TRNS,NLCK,TRNS,TRNS,TRNS,TRNS),
-    /* Rat */
-    [4] = KEYMAP(
+        TRNS,TRNS,TRNS,  NO,TRNS,                 NO,CAPS,  NO,TRNS,TRNS,TRNS),
+    [3] = KEYMAP(               /* Rat */
         TRNS,  NO,  NO,  NO,  NO,  NO,WH_L,WH_U,MS_U,WH_D,WH_R,  NO,  NO,
-        NO  ,  NO,  NO,  NO,  NO,  NO,BTN2,MS_L,MS_D,MS_R,BTN1,  NO,  NO,  NO,
+        TRNS,  NO,  NO,  NO,  NO,  NO,BTN2,MS_L,MS_D,MS_R,BTN1,  NO,  NO,TRNS,
         TRNS,  NO,  NO,  NO,  NO,  NO,BTN3,BTN2,BTN1,BTN4,BTN5,  NO,TRNS,  NO,
-        TRNS,TRNS,TRNS,  NO,                 NO,TRNS,BTN1,TRNS,TRNS,TRNS,TRNS),
-    /* Alt Gr */
-    [5] = KEYMAP(
-        ESC ,   Q,   W,   E,   R,   6,   7,   8,   9,   0,MINS,LBRC,RBRC,
-        TAB ,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT,NUHS, ENT,
-        LSFT,NUBS,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH,RSFT,BSPC,
-        LCTL,LGUI,LALT, FN0,                FN1, FN2, SPC,RALT,RGUI, APP,RCTL),
+        TRNS,TRNS,TRNS,  NO,  NO,               TRNS,BTN1,TRNS,TRNS,TRNS,TRNS),
+    [4] = KEYMAP(               /* Alt Gr */
+        TRNS,TRNS,TRNS,TRNS,TRNS,   6,   7,   8,   9,   0,MINS, EQL, GRV,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,  NO,  NO,                 NO,TRNS,TRNS,TRNS,TRNS,TRNS),
 };
 
 enum macro_id {
@@ -46,10 +35,9 @@ enum macro_id {
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1), /* Num */
     [1] = ACTION_LAYER_MOMENTARY(2), /* Nav */
-    [2] = ACTION_LAYER_MOMENTARY(4), /* Rat */
-    [3] = ACTION_LAYER_MOMENTARY(3), /* Num + Nav */
-    [4] = ACTION_MACRO(ALTGR),       /* Alt Gr; invoking macro */
-    [5] = ACTION_LAYER_MOMENTARY(5), /* Alt Gr; used in macro */
+    [2] = ACTION_LAYER_MOMENTARY(3), /* Rat */
+    [3] = ACTION_MACRO(ALTGR),       /* Alt Gr; invoking macro */
+    [4] = ACTION_LAYER_MOMENTARY(4), /* Alt Gr; used in macro */
 };
 
 /*
@@ -61,8 +49,8 @@ action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     switch (id) {
     case ALTGR:
         return (record->event.pressed ?
-                MACRO(D(RALT), D(FN5), END) :
-                MACRO(U(RALT), U(FN5), END));
+                MACRO(D(RALT), D(FN4), END) :
+                MACRO(U(RALT), U(FN4), END));
         break;
     }
     return MACRO_NONE;
